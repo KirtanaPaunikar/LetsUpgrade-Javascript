94c3f535d8115c17ebdf3ead38c1ce5000380fbb
Kirtana Arun Paunikar <68144132+KirtanaPaunikar@users.noreply.github.com>
2020-07-24 19:55:02 +0530

    Add files via upload

---
+++ Day 4/jscalculator.js
@@ -0,0 +1,22 @@
+function dis(val)
+{
+    document.getElementById("text").value += val;
+}
+
+function solve()
+{
+    let x= document.getElementById("text").value;
+    let y= eval(x);
+    document.getElementById("text").value= y;
+}
+
+function clr()
+{
+    document.getElementById("text").value = ""
+}
+
+// Only i am hving a problem in percentage
+// function percentage()
+// {
+
+// }
